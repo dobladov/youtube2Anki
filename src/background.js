55eecc6921e7c5f9cf2ba4e9c1876c7b43ff5fa2
Daniel Doblado <danieldoblado@gmail.com>
2021-06-11 23:56:05 +0200

    Update background script to v3

--- src/background.js
+++ src/background.js
@@ -1,14 +1,14 @@
 const enableDisableTab = (id, url) => {
   const re = /(http|https):\/\/(www.)?youtube\.com\\*/
   if (re.test(url)) {
-    chrome.browserAction.enable(id)
+    chrome.action.enable(id)
   } else {
-    chrome.browserAction.disable(id)
+    chrome.action.disable(id)
   }
 }
 
 chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const [activeTab] = tabs
     const { url, id } = activeTab
     enableDisableTab(id, url)
@@ -16,7 +16,7 @@ chrome.tabs.onActivated.addListener((tab) => {
 })
 
 chrome.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  if (changeInfo.status == "complete") {
-      enableDisableTab(id, tab.url)
-    }
+  if (changeInfo.status === 'complete') {
+    enableDisableTab(id, tab.url)
+  }
 })
