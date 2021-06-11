4879c582a28678f02f9e64c17859653015da216a
Daniel Doblado <danieldoblado@gmail.com>
2021-06-11 23:47:49 +0200

    Upgrade to manifest 3

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,17 +1,21 @@
-const connectionMessage = document.getElementById("connectionMessages")
-const decksList = document.getElementById("decksList")
-const deck = document.getElementById("deck")
+let deck
 
-const scheme = "http"
-const host = "localhost"
+const scheme = 'http'
+const host = 'localhost'
 const port = 8765
 
+/**
+ * Gets the name of the decks created in Anki
+ *
+ * @param {string} title
+ */
 const initDecks = async (title) => {
+  const decksList = document.getElementById('decksList')
   const deckNamesResponse = await fetch(`${scheme}://${host}:${port}`,
     {
       method: 'POST',
       body: JSON.stringify({
-        action: "deckNames",
+        action: 'deckNames',
         version: 6
       })
     }
@@ -20,7 +24,7 @@ const initDecks = async (title) => {
   !decks.result.includes(title) && decks.result.unshift(title)
 
   decks.result.forEach(name => {
-    const option = document.createElement("option")
+    const option = document.createElement('option')
     option.innerHTML = name
     option.value = name
     decksList.appendChild(option)
@@ -30,204 +34,220 @@ const initDecks = async (title) => {
 const getNotes = (subtitles, deck, model) => (
   subtitles.map(subtitle => (
     {
-      "deckName": deck,
-      "modelName": model,
-      "fields": {
-        "time": subtitle.time,
-        "nextTime": subtitle.nextTime || "",
-        "text": subtitle.text,
-        "prevText": subtitle.prevText || "",
-        "nextText": subtitle.nextText || "",
-        "id": subtitle.id,
-        "startSeconds": (subtitle.startSeconds && subtitle.startSeconds.toString()) || "",
-        "endSeconds": (subtitle.endSeconds && subtitle.endSeconds.toString()) || "",
-        "title": subtitle.title
+      deckName: deck,
+      modelName: model,
+      fields: {
+        time: subtitle.time,
+        nextTime: subtitle.nextTime || '',
+        text: subtitle.text,
+        prevText: subtitle.prevText || '',
+        nextText: subtitle.nextText || '',
+        id: subtitle.id,
+        startSeconds: (subtitle.startSeconds && subtitle.startSeconds.toString()) || '',
+        endSeconds: (subtitle.endSeconds && subtitle.endSeconds.toString()) || '',
+        title: subtitle.title
       },
-      "tags": [],
-      "options": {
-          "allowDuplicate": false
+      tags: [],
+      options: {
+        allowDuplicate: false
       }
     }
   ))
 )
 
-const sendNotification = (title, message, fn) => {
-  chrome.notifications.create("youtube2anki", {
-    "type": "basic",
-    "iconUrl": chrome.extension.getURL("icons/icon128.png"),
+/**
+ *  Creates a new notification with the given parameters
+ *
+ * @param {string} title
+ * @param {string} message
+ * @param {VoidFunction} callback
+ */
+const sendNotification = (title, message, callback) => {
+  const id = '_' + Math.random().toString(36).substr(2, 9)
+
+  chrome.notifications.create(id, {
+    type: 'basic',
+    iconUrl: chrome.runtime.getURL('icons/icon128.png'),
     title,
     message
   }, () => {
-    fn && fn()
+    callback && callback()
   })
 }
 
-chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-  const [activeTab] = tabs
-  const { id } = activeTab
+/**
+ * Set events and logic for the popup
+ */
+const initPopup = () => {
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    const activeTab = tabs[0]
 
-  chrome.tabs.sendMessage(id, {type: "getSubtitles"}, async (response) => {
+    // Initiate subtitles loading
+    chrome.tabs.sendMessage(activeTab.id, { type: 'getSubtitles' }, async (response) => {
+      const { title, subtitles } = response
 
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError.message)
-      return
-    }
+      if (title && subtitles) {
+        const downloadBtn = document.getElementById('downloadBtn')
 
-    const { title, subtitles } = response
+        downloadBtn.addEventListener('click', () => {
+          chrome.tabs.sendMessage(activeTab.id,
+            {
+              type: 'download',
+              title,
+              subtitles
+            }
+          )
+        })
 
-    if (title && subtitles) {
-      const downloadBtn = document.getElementById("downloadBtn")
+        const sendToAnkiForm = document.getElementById('sendToAnkiForm')
 
-      downloadBtn.addEventListener("click", () => {
-        chrome.tabs.sendMessage(id,
-          {
-            type: "download",
-            title,
-            subtitles
-          }
-        )
-      })
+        try {
+          await initDecks(title)
+          const connectionMessage = document.getElementById('connectionMessage')
 
-      const sendToAnkiForm = document.getElementById("sendToAnkiForm")
+          deck = document.getElementById('deck')
 
-      try {
-        await initDecks(title)
-        connectionMessage.classList.add("hide")
-        sendToAnkiForm.classList.remove("hide")
+          connectionMessage.classList.add('hide')
+          sendToAnkiForm.classList.remove('hide')
 
-        sendToAnkiForm.addEventListener("submit", async (e) => {
-          e.preventDefault()
-          sendToAnkiForm.classList.add("hide")
+          sendToAnkiForm.addEventListener('submit', async (e) => {
+            e.preventDefault()
+            sendToAnkiForm.classList.add('hide')
 
-          const selectedDeck = deck.value
+            const selectedDeck = deck.value
 
-          try {
-            const createDeckResponse = await fetch(`${scheme}://${host}:${port}`,
-              {
-                method: 'POST',
-                body: JSON.stringify({
-                  "action": "createDeck",
-                  "version": 6,
-                  "params": {
-                    "deck": selectedDeck
-                  }
-                })
-              }
-            )
+            try {
+              const createDeckResponse = await fetch(`${scheme}://${host}:${port}`,
+                {
+                  method: 'POST',
+                  body: JSON.stringify({
+                    action: 'createDeck',
+                    version: 6,
+                    params: {
+                      deck: selectedDeck
+                    }
+                  })
+                }
+              )
 
-            const {res, err} = await createDeckResponse.json()
-            if (err) throw new Error(err)
+              const { err } = await createDeckResponse.json()
+              if (err) throw new Error(err)
 
-            const createModelResponse = await fetch(`${scheme}://${host}:${port}`,
-              {
-                method: 'POST',
-                body: JSON.stringify({
-                  "action": "createModel",
-                  "version": 6,
-                  "params": {
-                      "modelName": "Youtube2Anki",
-                      "inOrderFields": [
-                        "time",
-                        "nextTime",
-                        "text",
-                        "prevText",
-                        "nextText",
-                        "id",
-                        "startSeconds",
-                        "endSeconds",
-                        "title",
+              const createModelResponse = await fetch(`${scheme}://${host}:${port}`,
+                {
+                  method: 'POST',
+                  body: JSON.stringify({
+                    action: 'createModel',
+                    version: 6,
+                    params: {
+                      modelName: 'Youtube2Anki',
+                      inOrderFields: [
+                        'time',
+                        'nextTime',
+                        'text',
+                        'prevText',
+                        'nextText',
+                        'id',
+                        'startSeconds',
+                        'endSeconds',
+                        'title'
                       ],
-                      "css": `
-                        .card {
-                          font-family: futura-pt,sans-serif,sans-serif;
-                          font-size: 20px;
-                          text-align: center;
-                          color: black;
-                          background-color: #e9e9e9;
+                      css: `
+                            .card {
+                              font-family: futura-pt,sans-serif,sans-serif;
+                              font-size: 20px;
+                              text-align: center;
+                              color: black;
+                              background-color: #e9e9e9;
+                            }
+
+                            span {
+                              font-size: 0.9rem;
+                              color: #3c3c3c;
+                            }
+                          `,
+                      cardTemplates: [
+                        {
+                          Front: `
+                                    {{title}}
+                                    <br>
+
+                                    <span>{{prevText}}</span>
+                                    <br>
+
+                                    {{text}}
+                                    <br>
+
+                                    <iframe
+                                        width="560"
+                                        height="315"
+                                        src="https://www.youtube.com/embed/{{id}}?start={{startSeconds}}&end={{endSeconds}}&autoplay=1"
+                                        frameborder=0
+                                          autoplay=1
+                                    />
+
+                                    <br>
+                                    <span>{{time}} - {{nextTime}}</span>
+                                    <br>
+                                    <span>{{nextText}}</span>
+                                  `,
+                          Back: `
+                                    {{FrontSide}}
+                                    <hr id=answer>
+                                  `
                         }
-
-                        span {
-                          font-size: 0.9rem;
-                          color: #3c3c3c;
-                        }
-                      `,
-                      "cardTemplates": [
-                          {
-                              "Front": `
-                                {{title}}
-                                <br>
-
-                                <span>{{prevText}}</span>
-                                <br>
-
-                                {{text}}
-                                <br>
-
-                                <iframe
-                                    width="560"
-                                    height="315"
-                                    src="https://www.youtube.com/embed/{{id}}?start={{startSeconds}}&end={{endSeconds}}&autoplay=1"
-                                    frameborder=0
-                                      autoplay=1
-                                />
-
-                                <br>
-                                <span>{{time}} - {{nextTime}}</span>
-                                <br>
-                                <span>{{nextText}}</span>
-                              `,
-                              "Back": `
-                                {{FrontSide}}
-                                <hr id=answer>
-                              `
-                          }
                       ]
-                  }
-                })
-              }
-            )
+                    }
+                  })
+                }
+              )
 
-            const {errorCreateModel} = await createModelResponse.json()
-            if (errorCreateModel) throw new Error(errorCreateModel)
+              const { errorCreateModel } = await createModelResponse.json()
+              if (errorCreateModel) throw new Error(errorCreateModel)
 
-            const addNotesResponse = await fetch(`${scheme}://${host}:${port}`,
-              {
-                method: 'POST',
-                body: JSON.stringify({
-                  "action": "addNotes",
-                  "version": 6,
-                  "params": {
-                    "notes":  getNotes(subtitles, selectedDeck, "Youtube2Anki")
-                  }
-                })
-              }
-            )
-            const {result, error} = await addNotesResponse.json()
-            if (error) throw new Error(error)
+              const addNotesResponse = await fetch(`${scheme}://${host}:${port}`,
+                {
+                  method: 'POST',
+                  body: JSON.stringify({
+                    action: 'addNotes',
+                    version: 6,
+                    params: {
+                      notes: getNotes(subtitles, selectedDeck, 'Youtube2Anki')
+                    }
+                  })
+                }
+              )
+              const { error } = await addNotesResponse.json()
+              if (error) throw new Error(error)
 
-            sendNotification(
-              chrome.i18n.getMessage("extensionName"),
-              chrome.i18n.getMessage("sucessCreatingCards")
+              sendNotification(
+                chrome.i18n.getMessage('extensionName'),
+                chrome.i18n.getMessage('successCreatingCards'),
+                () => window.close()
               )
             } catch (error) {
               sendNotification(
-              chrome.i18n.getMessage("errorSendingCards"),
-              error.message,
-            )
-          }
-        })
-
-      } catch(error) {
-        // Failed to get decks, meaning can't connect to anki
-        console.error(error)
+                chrome.i18n.getMessage('errorSendingCards'),
+                error.message
+              )
+            }
+          })
+        } catch (error) {
+          // Failed to get decks, meaning can't connect to Anki
+          console.error(error)
+        }
+      } else {
+        // Transcript was not open or found
+        sendNotification(
+          chrome.i18n.getMessage('errorTranscriptNotFoundTitle'),
+          chrome.i18n.getMessage('errorTranscriptNotFound'),
+          () => window.close()
+        )
       }
-
-    } else {
-      sendNotification(
-        chrome.i18n.getMessage("errorTranscriptNotFoundTitle"),
-        chrome.i18n.getMessage("errorTranscriptNotFound"),
-        () => window.close()
-      )
-    }
+    })
   })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  initPopup()
 })
--- src/youtube2Anki.js
+++ src/youtube2Anki.js
@@ -1,15 +1,21 @@
+/**
+ * Returns the given milliseconds in seconds
+ *
+ * @param {number} ms amount of milliseconds
+ * @returns {number}
+ */
 const toSeconds = (ms) => {
   if (ms) {
-    const a = ms.split(":")
+    const a = ms.split(':')
     return (+a[0] * 60) + (+a[1])
   }
 }
 
 const toCSV = (objArray) => {
   const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray
-  let str = ``
+  const str = ''
   return array.reduce((str, next) => {
-    str += `${Object.values(next).map(value => `"${value}"`).join(",")}` + '\r\n'
+    str += `${Object.values(next).map(value => `"${value}"`).join(',')}` + '\r\n'
     return str
   }, str)
 }
@@ -26,13 +32,13 @@ const download = (filename, text) => {
 
 const getSubtitles = (cues) => {
   return cues.map((cue, i) => {
-    const time = cue.querySelector(".cue-group-start-offset").innerText
-    const nextTime = (cues[i + 1]
-      && cues[i + 1].querySelector(".cue-group-start-offset").innerText
+    const time = cue.querySelector('.cue-group-start-offset').innerText
+    const nextTime = (cues[i + 1] &&
+      cues[i + 1].querySelector('.cue-group-start-offset').innerText
     ) || null
-    const text = cue.querySelector(".cue").innerText
-    const prevText = (cues[i - 1] && cues[i - 1].querySelector(".cue").innerText) || null
-    const nextText = (cues[i + 1] && cues[i + 1].querySelector(".cue").innerText) || null
+    const text = cue.querySelector('.cue').innerText
+    const prevText = (cues[i - 1] && cues[i - 1].querySelector('.cue').innerText) || null
+    const nextText = (cues[i + 1] && cues[i + 1].querySelector('.cue').innerText) || null
 
     return {
       time,
@@ -43,42 +49,48 @@ const getSubtitles = (cues) => {
       id: getID(window.location.href),
       startSeconds: toSeconds(time),
       endSeconds: toSeconds(nextTime) && (toSeconds(nextTime) + 1),
-      title: document.querySelector("h1").firstChild.innerText
+      title: document.querySelector('h1').firstChild.innerText
     }
   })
 }
 
 const getID = (url) => {
   let ID = ''
-  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
-  if(url[2] !== undefined) {
-    ID = url[2].split(/[^0-9a-z_\-]/i)
+  url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
+  if (url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_-]/i)
     ID = ID[0]
-  }
-  else {
+  } else {
     ID = url
   }
   return ID
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const { type } = request
+/**
+ * Listen to messages from popup
+ */
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    const { type } = request
 
-  if (type === 'getSubtitles') {
-    const cues = [...document.querySelectorAll(".cue-group")]
+    if (type === 'getSubtitles') {
+      const cues = [...document.querySelectorAll('.cue-group')]
 
-    if (cues.length) {
-      const title = document.querySelector("h1").firstChild.innerText
-      const subtitles = getSubtitles(cues)
-      sendResponse({title, subtitles})
-    } else {
-      sendResponse({subtitles: null, title: null})
+      if (cues.length) {
+        const title = document.querySelector('h1').firstElementChild.innerText
+        const subtitles = getSubtitles(cues)
+        console.log({ title, subtitles })
+        sendResponse({ title, subtitles })
+      } else {
+        console.log('Nothing found')
+        sendResponse({ subtitles: null, title: null })
+      }
     }
-  }
 
-  if (type === 'download') {
+    if (type === 'download') {
       const { title, subtitles } = request
       const csv = toCSV(subtitles)
       download(`${title}.csv`, csv)
+    }
   }
-})
+)
