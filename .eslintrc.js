0a75e93fe2dca0d4d6e04edebea95ea794adb1f0
Daniel Doblado <danieldoblado@gmail.com>
2021-06-11 23:46:48 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  globals: {
+    chrome: 'readonly'
+  },
+  rules: {
+  }
+}
