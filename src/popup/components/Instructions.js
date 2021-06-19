1ca8cd1dfee919d482c41404e9b9f1feac5b0a22
Daniel Doblado <danieldoblado@gmail.com>
2021-06-19 18:27:20 +0200

    Optimize image

--- src/popup/components/Instructions.js
+++ src/popup/components/Instructions.js
@@ -1,5 +1,6 @@
-import { div, h2, p, img, css } from '../skruv/html.js'
+import { div, h2, p, img, css, picture, source } from '../skruv/html.js'
 
+// @ts-ignore
 const style = css`
   img {
     max-width: 450px;
@@ -9,6 +10,19 @@ const style = css`
 export const Instructions = () => div({},
   h2({}, 'Transcript not found'),
   p({}, 'Please, open the transcript of the video'),
-  img({ src: chrome.runtime.getURL('instructions/openTranscript.png') }),
+  picture(
+    {},
+    source({
+      srcset: chrome.runtime.getURL('instructions/openTranscript.avif'),
+      type: 'image/avif'
+    }),
+    source({
+      srcset: chrome.runtime.getURL('instructions/openTranscript.webp'),
+      type: 'image/webp'
+    }),
+    img({
+      src: chrome.runtime.getURL('instructions/openTranscript.png')
+    })
+  ),
   style
 )
