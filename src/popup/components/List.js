c86e7740f393658e4022fcd3a436dd5fb2e943d6
Daniel Doblado <danieldoblado@gmail.com>
2022-08-11 23:15:42 +0200

    Merge cards (#28)

--- src/popup/components/List.js
+++ src/popup/components/List.js
@@ -3,6 +3,66 @@ import { div, css, ul, li, button, text, h2 } from '../skruv/html.js'
 import { state as mainState } from '../state.js'
 import { getEnabledSubtitles } from '../utils.js'
 
+/**
+ * Gets all the cards that will be merged
+ * according to the current selection
+ */
+const getCardsToMerge = () => {
+  if (isNaN(mainState.mergeStart) || isNaN(mainState.mergeEnd)) {
+    return []
+  }
+
+  if (mainState.mergeStart <= mainState.mergeEnd) {
+    return mainState.subtitles
+      .slice(mainState.mergeStart, mainState.mergeEnd + 1)
+      .map(v => ({ ...v }))
+  }
+
+  return mainState.subtitles
+    .slice(mainState.mergeEnd, mainState.mergeStart + 1)
+    .map(v => ({ ...v }))
+}
+
+/**
+ * Generates a single card from cardsToMerge
+ * and replaces it in the subtitles state
+ *
+ * @param {Subtitle[]} cardsToMerge
+ */
+const mergeCards = (cardsToMerge) => {
+  const lastCard = cardsToMerge[cardsToMerge.length - 1]
+  const card = {
+    ...cardsToMerge[0],
+    text: cardsToMerge.map(({ text }) => text).join(' '),
+    endSeconds: lastCard.endSeconds,
+    nextTime: lastCard.nextTime,
+    nextText: lastCard.nextText
+  }
+
+  // Insert the new card in between the selection indexes
+  if (mainState.mergeStart < mainState.mergeEnd) {
+    mainState.subtitles = [
+      ...mainState.subtitles.slice(0, mainState.mergeStart),
+      card,
+      ...mainState.subtitles.slice(mainState.mergeEnd + 1, mainState.subtitles.length)
+    ];
+
+    /** @type {HTMLElement} */(document.querySelector(`[data-index="${mainState.mergeStart}"]`))?.scrollIntoView({ behavior: 'smooth' })
+  } else {
+    mainState.subtitles = [
+      ...mainState.subtitles.slice(0, mainState.mergeEnd),
+      card,
+      ...mainState.subtitles.slice(mainState.mergeStart + 1, mainState.subtitles.length)
+    ];
+
+    /** @type {HTMLElement} */(document.querySelector(`[data-index="${mainState.mergeEnd}"]`))?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  // Reset selection
+  mainState.mergeStart = NaN
+  mainState.mergeEnd = NaN
+}
+
 // @ts-ignore
 const styling = css`
   .container  {
@@ -12,7 +72,7 @@ const styling = css`
     padding-right: 0;
     align-items: center;
     padding: 10px 0;
-    gap: 10px;
+    gap: 0.5rem;
   }
 
   .controls {
@@ -27,7 +87,10 @@ const styling = css`
     margin: 0;
     padding: 0;
     overflow-x: auto;
-    padding: 1px;
+  }
+  
+  li {
+    position: relative;
   }
 
   .text {
@@ -44,18 +107,73 @@ const styling = css`
     width: 100%;
     display: flex;
     align-items: center;
-    border-bottom: 1px solid var(--main-bg-color);
     cursor: pointer;
     margin: 0;
     padding: 10px;
     border: none;
     background: transparent;
-    outline-color: var(--action-color)
+    outline: none;
+    transition: background .2s ease-in-out;
   }
 
-  .listButton:focus,
-  .listButton:hover {
+  .listButton:focus {
     background-color: var(--main-bg-color);
+    box-shadow: inset 0 0 0 2px var(--action-color);
+    border-radius: 4px;
+    z-index: 2;
+  }
+
+  .inRange {
+    background-color: var(--action-disabled-color) !important;
+    cursor: copy;
+  }
+
+  li:focus-within .floating {
+    opacity: 1;
+    pointer-events: all;
+  }
+
+  .floating {
+    padding: .3rem 1rem;
+    pointer-events: none;
+    opacity: 0;
+    z-index: 3;
+    position: absolute;
+    bottom: -0.5rem;
+  }
+
+  .left {
+    left: 0.5rem;
+  }
+
+  .right {
+    right: 0.5rem;
+  }
+
+  .hidden {
+    transition: opacity .5s ease-in-out;
+    pointer-events: none;
+    opacity: 0 !important;
+  }
+
+  .reset {
+    position: absolute;
+    left: 50%;
+    top: .2rem;
+    border: none;
+    background: none;
+    color: var(--p-color);
+    transform: translateX(-50%);
+    font-size: .7rem;
+  }
+
+  .reset:hover {
+    color: var(--action-color);
+  }
+
+  .selectBtn {
+    padding-top: .2rem;
+    padding-bottom: .2rem;
   }
 `
 
@@ -89,9 +207,14 @@ const setRandom = () => {
   })
 }
 
-export const List = () => {
+/**
+ *
+ * @param {string} storageId
+ */
+export const List = (storageId) => {
   // const enabledCards = mainState.subtitles.filter(item => !item.disabled).length
   const enabledCards = getEnabledSubtitles(mainState.subtitles).length
+  const cardsToMerge = getCardsToMerge()
 
   return div(
     {
@@ -104,16 +227,16 @@ export const List = () => {
       h2({}, 'Select'),
       button({
         disabled: enabledCards === mainState.subtitles.length,
-        class: 'btn',
+        class: 'btn selectBtn',
         onclick: () => setAll(false)
       }, 'All'),
       button({
-        class: 'btn',
+        class: 'btn selectBtn',
         disabled: enabledCards === 0,
         onclick: () => setAll(true)
       }, 'None'),
       button({
-        class: 'btn',
+        class: 'btn selectBtn',
         onclick: setRandom
       }, 'Random')
     ),
@@ -123,7 +246,21 @@ export const List = () => {
           class: !!item.disabled && 'disabled'
         },
         button({
-          class: 'listButton',
+          onmouseover: (/** @type {{target: HTMLElement}}} */event) => {
+            event.target.focus()
+            // Select card for merge end if merge is started
+            if (!isNaN(mainState.mergeStart)) {
+              mainState.mergeEnd = i
+            }
+          },
+          onfocus: () => {
+            // Select card for merge with keyboard
+            if (!isNaN(mainState.mergeStart)) {
+              mainState.mergeEnd = i
+            }
+          },
+          'data-index': i,
+          class: `listButton${cardsToMerge.some(card => card.hash === item.hash) ? ' inRange' : ''}`,
           onclick: () => {
             toggleItem(i)
           }
@@ -138,8 +275,24 @@ export const List = () => {
         ),
         div({},
           item.nextTime
-        )
-        ))
+        )),
+        button({
+          class: 'btn floating left',
+          onclick: () => {
+            if (isNaN(mainState.mergeStart)) {
+              mainState.mergeStart = i
+            } else {
+              mainState.mergeStart = NaN
+              mainState.mergeEnd = NaN
+            }
+          }
+        }, isNaN(mainState.mergeStart) ? 'Merge' : 'Stop merge'),
+        button({
+          class: `btn floating right${cardsToMerge.length > 1 ? '' : ' hidden'}`,
+          onclick: () => {
+            mergeCards(cardsToMerge)
+          }
+        }, `Merge ${cardsToMerge.length} cards`))
       )),
       styling
     ),
@@ -156,6 +309,16 @@ export const List = () => {
       `Export ${enabledCards} cards`
         )
         : text({}, '⚠️ Select at least 1 card')
-    )
+    ),
+    button({
+      class: 'reset',
+      title: 'Delete stored cards and get new ones',
+      onclick: () => {
+        // Remove the storage and force a reload
+        chrome.tabs.sendMessage(mainState.activeTabId, { type: 'clearSubtitles', storageId }, async () => {
+          location.reload()
+        })
+      }
+    }, 'Delete saved cards')
   )
 }
